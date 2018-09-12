3ef2514ffe510782d77e439943e9ebbd0e56b97d
e <yi_curious@foxmail.com>
2018-09-12 19:19:04 +0800

    queues #3 commit bug fix

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+
 /**
  * Write a client program Permutation.java that takes an integer k as a command-line argument;
  * reads in a sequence of strings from standard input using StdIn.readString(); and prints
@@ -11,7 +12,8 @@ public class Permutation {
     public static void main(String[] args) {
         RandomizedQueue<String> queue = new RandomizedQueue<>();
         int k = Integer.parseInt(args[0]);
-        for (String s:StdIn.readLine().split(" ")){
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
             queue.enqueue(s);
         }
         for (int i = 0; i < k; i++) {
