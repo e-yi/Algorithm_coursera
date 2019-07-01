4cbb79ccfd87f2ce06ba1de5dc69fbdec44b8920
e-yi <e_yi@foxmail.com>
2019-07-01 17:13:24 +0800

    98/100 use MyTrieST to replace arg4 TrieST

--- src/BoggleSolver.java
+++ src/BoggleSolver.java
@@ -8,14 +8,71 @@ import java.util.Set;
 
 public class BoggleSolver {
 
-    private TrieST<Boolean> trieST;
+    private class MyTrieST{
+        private Node root;
+        private Set<String> wordSet = new HashSet<>();
+
+        class Node{
+            Node[] next = new Node[26];
+            boolean endOfWord = false;
+        }
+
+        MyTrieST(){}
+
+        boolean contains(String key){
+            return wordSet.contains(key);
+        }
+
+        void put(String key){
+//            assert key is in A-Z
+            if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+            else{
+                root = put(root, key, 0);
+                wordSet.add(key);
+            }
+        }
+
+        private Node put(Node x, String key, int d) {
+            if (x == null) x = new Node();
+            if(d == key.length()){
+                x.endOfWord = true;
+                return x;
+            }
+
+            int c = key.charAt(d)-'A';
+            x.next[c] = put(x.next[c], key, d+1);
+            return x;
+        }
+
+        Node get(String key){
+            return get(root, key,0);
+        }
+
+        Node get(Node x, String key, int d){
+            if (x==null){
+                return null;
+            }
+
+            if (d==key.length()){
+                return x;
+            }
+
+            int c = key.charAt(d)-'A';
+
+            return get(x.next[c],key,d+1);
+        }
+
+    }
+
+
+    private MyTrieST trieST;
 
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
-        trieST = new TrieST<>();
+        trieST = new MyTrieST();
         for (String word:dictionary) {
-            trieST.put(word, true);
+            trieST.put(word);
         }
     }
 
@@ -49,8 +106,8 @@ public class BoggleSolver {
      * @param set
      * @param board
      */
-    private void dfsBoard(int i,int j,String prefix,boolean[] path,
-                          Set<String> set,BoggleBoard board){
+    private void dfsBoard(int i, int j, String prefix, boolean[] path,
+                          Set<String> set, BoggleBoard board){
         // 合法性检查
         if (i<0||j<0||i>=board.rows()||j>=board.cols()){
             return;
@@ -68,13 +125,15 @@ public class BoggleSolver {
 
         String s = prefix.concat(tail);
 
+        MyTrieST.Node temp = trieST.get(s);
+
         // 检查是否有希望
-        if(!trieST.keysWithPrefix(s).iterator().hasNext()){
+        if (temp==null){
             return;
         }
 
         // 检查是否成功
-        if (trieST.get(s)!=null && s.length()>2){
+        if (temp.endOfWord && s.length()>2){
             set.add(s);
         }
 
